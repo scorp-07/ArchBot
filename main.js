2ce52c6ab4e2bc39672dad0ac0da4292395de382
scorp-07 <58151257+scorp-07@users.noreply.github.com>
2020-08-10 12:03:30 +0500

    JS app

---
+++ main.js
@@ -0,0 +1,5 @@
+if (navigator.onLine == true){
+  document.location.replace('https://90bb68c83ec5.ngrok.io');
+}else{
+  document.write('<main class="main"><section class="advantages"><div class="mess"><span class="mess_text">Нет подключения к интернету!</span></div><a class="button" href="./index.html">Обновить</a></section></main>');
+}
